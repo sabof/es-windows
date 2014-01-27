94114f07c35466fe1cdac046a5940c6892bd8959
sabof <esabof@gmail.com>
2014-01-27 03:19:04 +0000

    Allow creation of new buffers

--- es-windows.el
+++ es-windows.el
@@ -238,7 +238,7 @@ To prevent this message from showing, set `esw/be-helpful' to `nil'")
     selected-window))
 
 (defun esw/show-buffer (buffer)
-  (interactive (list (read-buffer "Choose buffer: ")))
+  (interactive (list (get-buffer-create (read-buffer "Choose buffer: "))))
   (set-window-buffer (esw/select-window) buffer))
 
 (defun esw/move-window (window)
